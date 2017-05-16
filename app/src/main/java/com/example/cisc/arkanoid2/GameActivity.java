fe9b1eaa52b2d836319c0fa8c5038cad01adad19
cisc <carlos.gomez5963@gmail.com>
2017-05-16 13:53:47 -0700

    Not done

--- app/src/main/java/com/example/cisc/arkanoid2/GameActivity.java
+++ app/src/main/java/com/example/cisc/arkanoid2/GameActivity.java
@@ -2,6 +2,18 @@ package com.example.cisc.arkanoid2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.media.SoundPool;
+import android.media.AudioAttributes;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,4 +22,19 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+  /*  public void playSound(int soundID){
+        SoundPool.play(soundMap.get(soundID), 1, 1, 1, 0, 1f);
+    }*/
